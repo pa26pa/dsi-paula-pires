f70308796e01c8908becd693d171ed88fb9405ca
Paula Pires <paula.pires2640@gmail.com>
2026-04-13 09:20:49 -0300

    aula04

---
+++ aula04/ExemplosVariaveis.java
@@ -0,0 +1,27 @@
+package aula04;
+
+public class ExemplosVariaveis {
+    public static void main(String[] args) {
+        double a;
+        a = 5 * 900 * 1212 / 75;
+        float b = 9020f;
+        int c = 100;
+        char d, e;
+        d = ':';
+        e = ')';
+        String f = "NOP!";
+        boolean ToComFome = false;
+        
+
+        if (ToComFome) {
+                f = "simmmmm";
+        }
+        
+
+        System.out.println("Valor de DOOBLE: " + a);
+        System.out.println("Valor de FLOAT: " + b);
+        System.out.println("Valor de INT: " + c);
+        System.out.println("Valor de CHAR + CHAR: " + d + e);
+        System.out.println("Valor de BOOLEAN (TO COM FOME?) --> " + f);
+    }
+}
