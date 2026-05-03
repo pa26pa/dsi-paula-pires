e4841b47dde8fd12a07eec0713c623d287a687d0
Paula Pires <paula.pires2640@gmail.com>
2026-05-03 13:11:49 -0300

    LIção para o dia 04/05/26

---
+++ aula05/Licao.java
@@ -0,0 +1,37 @@
+package aula05;
+
+public class Licao {
+    public static void main(String[] args) {
+        // oque são assignment operators?
+        // são operadores que são utilizados para dar valor para uma variavel
+        // = , += , -=, *=, /=, %=, &= |=, ^=, >>=, <<=
+
+        // Utilizando os operadores:
+
+        int a = 10;
+        a = 9;
+        System.out.println(a);
+        a += 1;
+        System.out.println(a);
+        a -= 2;
+        System.out.println(a);
+        a *= 2;
+        System.out.println(a);
+        a /= 2;
+        System.out.println(a);
+        a %= 2;
+        System.out.println(a);
+        a &= 3;
+        System.out.println(a);
+        a |= 3;
+        System.out.println(a);
+        a ^= 2;
+        System.out.println(a);
+        a >>= 4;
+        System.out.println(a);
+        a <<= 10;
+        System.out.println(a);
+
+
+    }
+}
