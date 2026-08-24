d0b00e405b11edadf401a9c6e6ad5408b4739de5
Paula Pires <paula.pires2640@gmail.com>
2026-08-24 09:28:31 -0300

    aula08 lembrar de fazer lição

---
+++ aula08/src/test/java/aula/com/aula08/Aula08ApplicationTests.java
@@ -0,0 +1,13 @@
+package aula.com.aula08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
