f73d01404f5579e73635fc6c0d2ecc1450c9bd40
Paula Pires <paula.pires2640@gmail.com>
2026-08-30 21:30:02 -0300

    Lição para 31/08/2026 não sei se está funcionando

---
+++ demo/src/main/java/com/example/RestServiceApplication.java
@@ -0,0 +1,11 @@
+package demo.src.main.java.com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public RestServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args)
+    }
+}
---
+++ demo/src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,3 @@
+package demo.src.main.java.com.example.demo;
+
+public record Greeting(long id, String content) { }
